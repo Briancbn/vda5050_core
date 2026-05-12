76fe5505a9dfc2c8f4e692b5d537c6bca2dcfb4e
Lim Yew Hao <lim_yew_hao@a-star.edu.sg>
2026-05-12 21:16:19 +0800

    feat(core): add broker connection-state callbacks to MqttClientInterface
    Signed-off-by: Lim Yew Hao <lim_yew_hao@a-star.edu.sg>

--- vda5050_core/include/vda5050_core/transport/paho_mqtt_client.hpp
+++ vda5050_core/include/vda5050_core/transport/paho_mqtt_client.hpp
@@ -124,6 +124,12 @@ public:
   void set_will(
     const std::string& topic, const std::string& message, int qos) override;
 
+  // Documentation inherited from MqttClientInterface
+  void set_connection_lost_callback(ConnectionStateHandler handler) override;
+
+  // Documentation inherited from MqttClientInterface
+  void set_connected_callback(ConnectionStateHandler handler) override;
+
   /// \brief Get a mutable reference to Paho configuration options
   ///
   /// \return Mutable reference to Paho configuration options
@@ -153,6 +159,14 @@ private:
 
   /// \brief Mutex protecting list of message handlers
   std::mutex handler_mutex_;
+
+  /// \brief Optional handler invoked when broker connection is lost
+  /// (Task #70). Stored under handler_mutex_.
+  ConnectionStateHandler connection_lost_handler_;
+
+  /// \brief Optional handler invoked when broker connection is
+  /// (re)established (Task #70). Stored under handler_mutex_.
+  ConnectionStateHandler connected_handler_;
 
   /// \brief MQTT connection options
   mqtt::connect_options conn_options_;
