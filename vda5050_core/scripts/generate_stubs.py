140e71e73a3dbda1b75a829f598975b186412acf
Chen Bainian <chenbn@a-star.edu.sg>
2026-08-17 15:17:46 +0800

    chore(pybind): add custom post processing script for stubgen
    Signed-off-by: Chen Bainian <chenbn@a-star.edu.sg>

---
+++ vda5050_core/scripts/generate_stubs.py
@@ -0,0 +1,73 @@
+import glob
+import sys
+from pathlib import Path
+
+from pybind11_stubgen import main as stubgen_main
+
+DIR = Path(__file__).parent.resolve()
+HEADER_FILE = DIR / "stub_header.txt"
+
+
+def _get_header(header_file):
+    header = ""
+    with open(header_file, "r") as f:
+        for line in f:
+            # strip trailing white space
+            header += ("# " + line.rstrip("\n").rstrip("\r")).rstrip()
+            header += "\n"
+
+    return header
+
+
+def _add_header(header, file):
+    with open(file, "r+") as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(header + "\n" + content)
+
+
+def _get_outout_dir():
+    for i in range(len(sys.argv)):
+        # reverse the search for the last defined option
+        index = len(sys.argv) - i - 1
+        value = sys.argv[index]
+
+        if value == "-o" or value == "--output-dir":
+            break
+
+    # Set to default value
+    if index == 0:
+        return "./stub"
+
+    return sys.argv[index + 1]
+
+
+def _is_dry_run():
+    return "--dry-run" in sys.argv
+
+
+def post_main():
+    if _is_dry_run():
+        return
+
+    print("post-processing: adding headers...")
+
+    output_dir = _get_outout_dir()
+    header = _get_header(HEADER_FILE)
+    print(f"headers found in {HEADER_FILE}")
+
+    # Find all .log files recursively
+    print(f"processing stub files (.pyi) in {output_dir}")
+    for file_path in glob.iglob(f"{output_dir}/**/*.pyi", recursive=True):
+        _add_header(header, file_path)
+
+    print("post-processing: done")
+
+
+def main():
+    stubgen_main()
+    post_main()
+
+
+if __name__ == "__main__":
+    main()
