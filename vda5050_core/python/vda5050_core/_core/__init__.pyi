9821ca01aebdbc9ecac100558b28b0b8d29048dd
Chen Bainian <chenbn@a-star.edu.sg>
2026-08-17 11:52:42 +0800

    feat(pybind): add transport bindings
    Signed-off-by: Chen Bainian <chenbn@a-star.edu.sg>

--- vda5050_core/python/vda5050_core/_core/__init__.pyi
+++ vda5050_core/python/vda5050_core/_core/__init__.pyi
@@ -9,4 +9,5 @@ from __future__ import annotations
 from . import client
 from . import master
 from . import rmf_migration
-__all__: list[str] = ['client', 'master', 'rmf_migration']
+from . import transport
+__all__: list[str] = ['client', 'master', 'rmf_migration', 'transport']
