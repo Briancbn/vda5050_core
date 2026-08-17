97ecee7c32b7ad68749092d8c281a8ac93857f23
Chen Bainian <chenbn@a-star.edu.sg>
2026-08-17 11:05:17 +0800

    feat(pybind): add initial master bindings
    Signed-off-by: Chen Bainian <chenbn@a-star.edu.sg>

--- vda5050_core/python/vda5050_core/__init__.pyi
+++ vda5050_core/python/vda5050_core/__init__.pyi
@@ -4,6 +4,7 @@
 
 from __future__ import annotations
 from vda5050_core._core import client
+from vda5050_core._core import master
 from vda5050_core._core import rmf_migration
 from . import _core
-__all__: list[str] = ['client', 'rmf_migration']
+__all__: list[str] = ['client', 'master', 'rmf_migration']
--- vda5050_core/python/vda5050_core/_core/__init__.pyi
+++ vda5050_core/python/vda5050_core/_core/__init__.pyi
@@ -7,5 +7,6 @@ VDA5050 Core Python bindings
 """
 from __future__ import annotations
 from . import client
+from . import master
 from . import rmf_migration
-__all__: list[str] = ['client', 'rmf_migration']
+__all__: list[str] = ['client', 'master', 'rmf_migration']
